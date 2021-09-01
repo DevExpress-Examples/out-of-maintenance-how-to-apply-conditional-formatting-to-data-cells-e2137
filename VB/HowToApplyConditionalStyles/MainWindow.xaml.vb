e91c681a0a576c4125148a49eb91a7d363ca13a4
DevExpressExampleBot <fakeEmail>
2021-09-01 18:37:04 +0300

    autogenerate VB

--- VB/HowToApplyConditionalStyles/MainWindow.xaml.vb
+++ VB/HowToApplyConditionalStyles/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
+﻿Imports System.Data
 Imports System.Data.OleDb
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
@@ -10,48 +9,46 @@ Imports System
 Imports System.Windows.Media
 
 Namespace HowToBindToMDB
-    Partial Public Class MainWindow
-        Inherits Window
-        Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-        Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-            pivotGridControl1.DataSource = salesPersonDataTable
-        End Sub
+'INSTANT VB NOTE: The variable salesPersonDataTable was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
+		Private salesPersonDataTable_Conflict As New NwindDataSet.SalesPersonDataTable()
+		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
-        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            salesPersonDataAdapter.Fill(salesPersonDataTable)
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+			pivotGridControl1.DataSource = salesPersonDataTable_Conflict
+		End Sub
 
-    Public Class ValueToColorConverter
-        Inherits MarkupExtension
-        Implements IValueConverter
-        #Region "IValueConverter Members"
-        Public Function Convert(ByVal value As Object, _
-								ByVal targetType As System.Type, _
-								ByVal parameter As Object, _
-								ByVal culture As System.Globalization.CultureInfo) _
-								As Object Implements IValueConverter.Convert
-                If System.Convert.ToDecimal(value) < 10000 Then
-                    Return Brushes.Yellow
-                Else
-                    Return Brushes.Transparent
-                End If
-        End Function
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			salesPersonDataAdapter.Fill(salesPersonDataTable_Conflict)
+		End Sub
+	End Class
 
-        Public Function ConvertBack(ByVal value As Object, _
-									ByVal targetType As System.Type, _
-									ByVal parameter As Object, _
-									ByVal culture As System.Globalization.CultureInfo) _
-									As Object Implements IValueConverter.ConvertBack
-            Throw New System.NotImplementedException()
-        End Function
-        #End Region
+	Public Class ValueToColorConverter
+		Inherits MarkupExtension
+		Implements IValueConverter
 
-        Public Overrides Function ProvideValue(ByVal serviceProvider As System.IServiceProvider) As Object
-            Return Me
-        End Function
-    End Class
+		#Region "IValueConverter Members"
+		Public Function Convert(ByVal value As Object, ByVal targetType As System.Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+				If System.Convert.ToDecimal(value) < 10000 Then
+					Return Brushes.Yellow
+				Else
+					Return Brushes.Transparent
+				End If
+		End Function
+
+		Public Function ConvertBack(ByVal value As Object, ByVal targetType As System.Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Throw New System.NotImplementedException()
+		End Function
+		#End Region
+
+		Public Overrides Function ProvideValue(ByVal serviceProvider As System.IServiceProvider) As Object
+			Return Me
+		End Function
+	End Class
 End Namespace
